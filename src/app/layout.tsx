61621b6c03906ae0a42fad9273e3825aa256a1cc
Florian Winkler <flo@wnklr.art>
2023-11-03 22:42:42 +0100

    change order of wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<StyledComponentsRegistry>
-				<body className={inter.className}>{children}</body>
-			</StyledComponentsRegistry>
+			<body className={inter.className}>
+				<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+			</body>
 		</html>
 	);
 }
