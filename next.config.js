81cf6555188cab7e05eb1d68dbf4d96fdc8e67b4
Florian Winkler <flo@wnklr.art>
2023-11-03 21:58:28 +0100

    adjust next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+	},
+	webpack5: true,
+	webpack: (config) => {
+		config.resolve.fallback = {
+			fs: false,
+			net: false,
+			dns: false,
+			child_process: false,
+			tls: false,
+		};
 
-module.exports = nextConfig
+		return config;
+	},
+};
+
+module.exports = nextConfig;
