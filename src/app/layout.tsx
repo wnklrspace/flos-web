c1a5a5aa2a2051bcb3a667806ca600ff6992e931
Florian Winkler <flo@wnklr.art>
2023-11-03 22:50:56 +0100

    :(((((

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css';
 import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from './registry';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
