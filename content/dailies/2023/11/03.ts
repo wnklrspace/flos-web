703a22aedda53e8fc5e3587e70bc687c97863932
Florian Winkler <flo@wnklr.art>
2023-11-04 00:17:02 +0100

    content content

--- content/dailies/2023/11/03.ts
+++ content/dailies/2023/11/03.ts
@@ -3,11 +3,11 @@ import { MEDIA_TYPE } from '../../../../types/media';
 
 const Content: Daily = {
 	type: DAILY_TYPE.IMAGE,
-	title: 'November 2, 2023',
+	title: 'November 3, 2023',
 	description: '10 shades of soft feelings.',
 	media: {
 		type: MEDIA_TYPE.IMAGE,
-		url: '/dailies/2023/11/02.png',
+		url: '/dailies/2023/11/03.png',
 		alt: 'Feeling alone while feeling stupid.',
 		width: 2000,
 		height: 2000,
--- content/dailies/index.ts
+++ content/dailies/index.ts
@@ -1,4 +1,4 @@
 import { Daily } from '../../types/dailies';
-import Content from './2023/11/02';
+import Content from './2023/11/03';
 
 export const dailies: Daily[] = [Content];
