69abad7fd7a762aec0e0cfe850f80833ae1496c7
Florian Winkler <flo@wnklr.art>
2023-11-04 20:00:31 +0100

    change order

--- content/dailies/index.ts
+++ content/dailies/index.ts
@@ -3,4 +3,4 @@ import ShadesOfFeeling from './2023/11/03';
 import Slipped from './2023/11/04';
 import Vegetable from './2023/11/05';
 
-export const dailies: Daily[] = [Slipped, ShadesOfFeeling, Vegetable];
+export const dailies: Daily[] = [Vegetable, Slipped, ShadesOfFeeling];
