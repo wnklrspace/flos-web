892f25f981d327267ee6abc9b31543082d9ca9fb
Florian Winkler <flo@wnklr.art>
2023-11-04 20:00:18 +0100

    add new content

---
+++ content/dailies/2023/11/05.ts
@@ -0,0 +1,17 @@
+import { DAILY_TYPE, Daily } from '../../../../types/dailies';
+import { MEDIA_TYPE } from '../../../../types/media';
+
+const Vegetable: Daily = {
+	type: DAILY_TYPE.IMAGE,
+	title: 'November 5, 2023',
+	description: 'almost 27, almost old vegetable',
+	media: {
+		type: MEDIA_TYPE.IMAGE,
+		url: '/dailies/2023/11/04.jpeg',
+		alt: 'Drawing of a mixture between a men and a vegetable.',
+		width: 960,
+		height: 1280,
+	},
+};
+
+export default Vegetable;
--- content/dailies/index.ts
+++ content/dailies/index.ts
@@ -1,5 +1,6 @@
 import { Daily } from '../../types/dailies';
 import ShadesOfFeeling from './2023/11/03';
 import Slipped from './2023/11/04';
+import Vegetable from './2023/11/05';
 
-export const dailies: Daily[] = [Slipped, ShadesOfFeeling];
+export const dailies: Daily[] = [Slipped, ShadesOfFeeling, Vegetable];
