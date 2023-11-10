8a0f4016e7ff55dabd64ee2d65ebf214c2e74c14
Florian Winkler <flo@wnklr.art>
2023-11-10 03:18:21 +0100

    include poem post

--- content/dailies/index.ts
+++ content/dailies/index.ts
@@ -3,5 +3,12 @@ import ShadesOfFeeling from './2023/11/03';
 import Slipped from './2023/11/04';
 import Vegetable from './2023/11/05';
 import USO from './2023/11/09';
+import Poem from './2023/11/10';
 
-export const dailies: Daily[] = [USO, Vegetable, Slipped, ShadesOfFeeling];
+export const dailies: Daily[] = [
+	Poem,
+	USO,
+	Vegetable,
+	Slipped,
+	ShadesOfFeeling,
+];
