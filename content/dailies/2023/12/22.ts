5d274c9f1f40b3a3145d359d7fc1a1bdd6f80cc2
Florian Winkler <flo@wnklr.art>
2023-12-22 14:10:13 +0100

    feat: add daily for 22 dec

---
+++ content/dailies/2023/12/22.ts
@@ -0,0 +1,18 @@
+import { DAILY_TYPE, Daily } from '../../../../types/dailies';
+
+const Menge: Daily = {
+	type: DAILY_TYPE.TEXT,
+	text: [
+		`du dort im gedränge`,
+		`umringt von allen gefühlen, die noch da sind, nach all dem hin und her gerenne`,
+		`ich fange an zu zählen, doch komm nur auf ’ne kleine menge`,
+		`lös mich stück für stück von allem womit ich an dir hänge`,
+		`verlier‘ den überblick über alles, was ich für dich empfind`,
+		`wut übernimmt das steuer, sticht allen die augen aus und ich werd langsam blind`,
+		`werde wütend und schieb‘s auf alles was ich (nicht) erlebt hab als kind`,
+		`renne hundert runden in meim kopf und merke wie die erinnerung an dich schwimmt`,
+	],
+	title: 'Dezember 22, 2023',
+};
+
+export default Menge;
--- content/dailies/2023/12/index.ts
+++ content/dailies/2023/12/index.ts
@@ -1,6 +1,7 @@
+import Menge from './22';
 import VierzehnTage from './16';
 import VierzehnTageAlternative from './12-alternative';
 
-const December = [VierzehnTage, VierzehnTageAlternative];
+const December = [Menge, VierzehnTage, VierzehnTageAlternative];
 
 export default December;
