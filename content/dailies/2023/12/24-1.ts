e2be736932c35642c9e01f69c5d918b46a22ba34
Florian Winkler <flo@wnklr.art>
2023-12-24 22:17:18 +0100

    add song

---
+++ content/dailies/2023/12/24-1.ts
@@ -0,0 +1,9 @@
+import { DAILY_TYPE, Daily } from '../../../../types/dailies';
+
+const PaperThin: Daily = {
+	type: DAILY_TYPE.TEXT,
+	text: ['https://www.youtube.com/watch?v=4Mtjq7B0Z6Y'],
+	title: 'Dezember 24, 2023',
+};
+
+export default PaperThin;
--- content/dailies/2023/12/index.ts
+++ content/dailies/2023/12/index.ts
@@ -1,7 +1,8 @@
+import PaperThin from './24-1';
 import Vibration from './24';
 import VierzehnTage from './16';
 import VierzehnTageAlternative from './12-alternative';
 
-const December = [Vibration, VierzehnTage, VierzehnTageAlternative];
+const December = [PaperThin, Vibration, VierzehnTage, VierzehnTageAlternative];
 
 export default December;
