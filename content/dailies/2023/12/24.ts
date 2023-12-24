0a450e442b902268c1afc0b65ff58c1c820f80a5
Florian Winkler <flo@wnklr.art>
2023-12-24 22:12:52 +0100

    add poem

---
+++ content/dailies/2023/12/24.ts
@@ -0,0 +1,15 @@
+import { DAILY_TYPE, Daily } from '../../../../types/dailies';
+
+const Vibration: Daily = {
+	type: DAILY_TYPE.TEXT,
+	text: [
+		'Worte verlassen deinen Mund',
+		'Prallen an mir ab',
+		'Dein Kehlkopf, der immer lauter brummt',
+		'Weiss nicht, was du sagst',
+		'Warte so lang, bis unsere Konversation verstummt',
+	],
+	title: 'Dezember 24, 2023',
+};
+
+export default Vibration;
--- content/dailies/2023/12/index.ts
+++ content/dailies/2023/12/index.ts
@@ -1,6 +1,7 @@
+import Vibration from './24';
 import VierzehnTage from './16';
 import VierzehnTageAlternative from './12-alternative';
 
-const December = [VierzehnTage, VierzehnTageAlternative];
+const December = [Vibration, VierzehnTage, VierzehnTageAlternative];
 
 export default December;
