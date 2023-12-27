fb40f268c48bfcd1a512bea7937c2ba45a833e41
Florian Winkler <flo@wnklr.art>
2023-12-27 20:59:45 +0100

    add 27.12 content

---
+++ content/dailies/2023/12/27.ts
@@ -0,0 +1,15 @@
+import { DAILY_TYPE, Daily } from '../../../../types/dailies';
+
+const Windboe: Daily = {
+	type: DAILY_TYPE.TEXT,
+	text: [
+		'Eine Windboe reißt nacheinander Bäume um',
+		'Ich sitz in der zweiten Reihe nur noch krumm',
+		'krümm mich und bleibe stumm',
+		'Seitdem du nicht mehr da bist',
+		'Reißt eine Windboe nach der anderen Bäume um',
+	],
+	title: 'Dezember 27, 2023',
+};
+
+export default Windboe;
--- content/dailies/2023/12/index.ts
+++ content/dailies/2023/12/index.ts
@@ -1,8 +1,15 @@
+import Windboe from './27';
 import PaperThin from './24-1';
 import Vibration from './24';
 import VierzehnTage from './16';
 import VierzehnTageAlternative from './12-alternative';
 
-const December = [PaperThin, Vibration, VierzehnTage, VierzehnTageAlternative];
+const December = [
+	Windboe,
+	PaperThin,
+	Vibration,
+	VierzehnTage,
+	VierzehnTageAlternative,
+];
 
 export default December;
