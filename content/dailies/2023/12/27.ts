3a66dc0d51e8907150cb24c0f0ba07bb160d7b44
Florian Winkler <flo@wnklr.art>
2023-12-27 21:02:10 +0100

    fix content to 27.12

--- content/dailies/2023/12/27.ts
+++ content/dailies/2023/12/27.ts
@@ -3,9 +3,9 @@ import { DAILY_TYPE, Daily } from '../../../../types/dailies';
 const Windboe: Daily = {
 	type: DAILY_TYPE.TEXT,
 	text: [
-		'Eine Windboe reißt nacheinander Bäume um',
-		'Ich sitz in der zweiten Reihe nur noch krumm',
-		'krümm mich und bleibe stumm',
+		'Eine Windboe reißt Bäume um',
+		'Mein Kopf sitzt in der zweiten Reihe nur noch krumm',
+		'Krümmt sich und ich bleibe dabei stumm',
 		'Seitdem du nicht mehr da bist',
 		'Reißt eine Windboe nach der anderen Bäume um',
 	],
