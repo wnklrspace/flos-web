0945330b1e4e0dfdeb152b7fd5bf80719c6ff4d7
Florian Winkler <florian.winkler@muse-case.com>
2026-08-22 15:05:27 +0200

    chore: update nextjs version

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+export default defineConfig([...nextCoreWebVitals, ...nextTypescript]);
